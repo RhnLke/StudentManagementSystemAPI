71d40fd385133a9a230f9d3d327b8092d1b9f600
nohrl <nohrl@RLUCAS>
2024-07-12 20:00:52 +0800

    Add project files.

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static void AddInfrastructureServices(this IServiceCollection serviceDescriptors)
+        {
+            
+        }
+    }
+}
